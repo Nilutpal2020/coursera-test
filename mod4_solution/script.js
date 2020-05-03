77378ae10c673d0b17bf43ef7ffedaf4afd24bea
Nilutpal <alexnilut@gmail.com>
2020-05-03 16:46:08 +0530

    minor changes

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,11 +1,12 @@
 (function(window) {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i = 0; i < names.length; i++) {
-        var firstLetter = names[i].charAt(0).toLowerCase();
-        if ((firstLetter === "j")) {
-            byeSpeaker.speak(names[i]);
+    for (var name of names) {
+        var firstLetter = name.charAt(0).toLowerCase();
+
+        if (firstLetter === "j") {
+            byeSpeaker.speak(name);
         } else {
-            helloSpeaker.speak(names[i]);
+            helloSpeaker.speak(name);
         }
     }
     window.names = names;
